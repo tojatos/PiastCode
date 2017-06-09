5db527a61042a909af52f95f691a3e5513b36d47
tojatos <tojatos@gmail.com>
2017-06-10 00:25:30 +0200

    Event - create event (basic)

---
+++ application/views/Event/create_event_form.php
@@ -0,0 +1,10 @@
+<form class="create_event_form" type="POST">
+	Nazwa: <input type="text" name="name">
+	Opis: <input type="text" name="description">
+	Data start: <input type="text" name="date_start">
+	Godzina start: <input type="text" name="time_start">
+	Data end: <input type="text" name="date_end">
+	Godzina end: <input type="text" name="time_end">
+	Id miejsca: <input type="text" name="place_id">
+	<input type="submit" value="Wyślij mnie">
+</form>
