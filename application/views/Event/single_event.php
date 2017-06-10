95bb698758c29e93a320141ccf97d113f3ee3781
Unknown <maciej.bajda@mbajda.pro>
2017-06-10 04:13:24 +0200

    wydarzenie

--- application/views/Event/single_event.php
+++ application/views/Event/single_event.php
@@ -2,13 +2,14 @@
 
 
 <div class="event_offer">
-    <div class="title">Zbieranie ziemniaków</div>
-    <div class="date">10.06.2017</div>
-    <div class="place">CWK w Opolu</div>
+    <div class="image"><img src="<?= site_url("/public/images/zbieranie_ziemniakow.jpg") ?>"/></div>
+    
    
-    <div class="content">
-         <div class="image"><img src="<?= site_url("/public/images/zbieranie_ziemniakow.jpg") ?>"/></div>
-        <div class="content">Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton.</div>
+    <div class="event_content">
+        <div class="title">Zbieranie ziemniaków</div>
+        <div class="date">10.06.2017</div>
+        <div class="place">CWK w Opolu</div>
+        <div class="content">Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton. Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton.</div>
     </div>
 
 
