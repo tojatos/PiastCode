29d6047e75be8021ea7fb1fe5a460047a17cbe4b
Yarde <dawidkal66@gmail.com>
2017-06-10 06:14:15 +0200

    panel administratora

--- application/views/Administrator/index.php
+++ application/views/Administrator/index.php
@@ -1,3 +1,9 @@
+<div class="panel-heading">
+	               <div class="text-center">
+	               		<h1 class="title">Panel Administratora</h1>
+	               		<hr />
+	               	</div>
+</div>
 <div class="event_container">
     <div class="left_admin">
         <?= $create_category_form ?>
