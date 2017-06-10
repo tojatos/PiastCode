a432d44a1926f4a739b765b6f1f999826ef69b11
Yarde <dawidkal66@gmail.com>
2017-06-10 03:38:52 +0200

    buttons on nav

--- application/views/inc/mainNav.php
+++ application/views/inc/mainNav.php
@@ -4,6 +4,9 @@
   <ul class="navbar-left">
     <?php if ($this->session->is_logged): ?>
      <li><a href="<?= site_url('Event/Create')?>"class="<?= ($this->uri->segment(1)=="Event") ? "active" : ""?>">Dodawanie wydarzenia</a></li>
+        <?php if ($this->session->is_admin): ?>
+            <li><a href="<?= site_url('Administrator')?>"class="<?= ($this->uri->segment(1)=="Administrator") ? "active" : ""?>">Panel Administratora</a></li>
+        <?php endif; ?>
      <?php endif; ?>
   </ul>
   <ul class="navbar-right">
@@ -11,6 +14,7 @@
       <li><a href="<?= site_url('Register')?>" class="<?= ($this->uri->segment(1)=="Register") ? "active" : ""?>">Rejestracja</a></li>
       <li><a href="<?= site_url('Login')?>" class="<?= ($this->uri->segment(1)=="Login") ? "active" : ""?>">Logowanie</a></li>
     <?php else: ?>
+    <li><a href="<?= site_url('User')?>"class="<?= ($this->uri->segment(1)=="User") ? "active" : ""?>">Profil</a></li>
       <li>
         <form class="logout_form" method="post">
           <input class="logout" type="submit" value="Wyloguj">
