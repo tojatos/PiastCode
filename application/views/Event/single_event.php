0c3f4dff325ecd6ed090f6faeb0330388e78b10c
Unknown <maciej.bajda@mbajda.pro>
2017-06-10 03:52:31 +0200

    zbieranie ziemniaków

--- application/views/Event/single_event.php
+++ application/views/Event/single_event.php
@@ -3,8 +3,13 @@
 
 <div class="event_offer">
     <div class="title">Zbieranie ziemniaków</div>
-
-
+    <div class="date">10.06.2017</div>
+    <div class="place">CWK w Opolu</div>
+   
+    <div class="content">
+         <div class="image"><img src="<?= site_url("/public/images/zbieranie_ziemniakow.jpg") ?>"/></div>
+        <div class="content">Jak wiadomo zbieranie ziemniaków polega na ich wyrywaniu z ziemi. Największym problemem będzie przekopwanie się przez beton.</div>
+    </div>
 
 
 
